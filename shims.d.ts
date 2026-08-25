2005b91e9cecfd7187c6774c904dad3e3d05b0bf
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-25 21:01:25 +1000

    added shims.d.ts to try and cpp function

---
+++ shims.d.ts
@@ -0,0 +1,9 @@
+// Auto-generated. Do not edit.
+declare namespace OLED {
+
+    /** Set I2c Speed. */
+    //% block="I2C frequency %frequency Hz"
+    //% weight=100 shim=OLED::setI2CSpeed
+    function setI2CSpeed(): void;
+
+}
