dae7446914a76c58f0a8bfc81c0ffb39e8802764
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-25 20:54:08 +1000

    third commit

--- i2c.cpp
+++ i2c.cpp
@@ -1,9 +1,11 @@
 #include "pxt.h"
 
 namespace OLED {
-    //% block="set I2C speed to %speed Hz"
-    void setI2CSpeed(int speed) {
-        // Access CODAL's global I2C object and change frequency (e.g., 400000 for 400kHz)
-        uBit.i2c.setFrequency(speed); 
+
+    //% block="I2C frequency %frequency Hz"
+    void setI2CSpeed(int speed)
+    {
+        uBit.i2c.setFrequency(speed);
     }
+
 }
