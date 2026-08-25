ab6a12209502699376ebee29a9434e61adfe8250
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-25 21:37:02 +1000

    changed clear to fill on init

--- oled.ts
+++ oled.ts
@@ -305,7 +305,7 @@ namespace OLED {
         cmd1(0xA6)       // SSD1306_NORMALDISPLAY
         cmd2(0xD6, 0)    // zoom off
         cmd1(0xAF)       // SSD1306_DISPLAYON
-        clear()
+        fill(0)
     }
 
     init();
