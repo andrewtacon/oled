3ac675a92132f46841d0aa5bf32589d0c731e265
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-25 21:16:39 +1000

    attempt to fix shim

--- shims.d.ts
+++ shims.d.ts
@@ -4,6 +4,6 @@ declare namespace OLED {
     /** Set I2c Speed. */
     //% block="I2C frequency %frequency Hz"
     //% weight=100 shim=OLED::setI2CSpeed
-    function setI2CSpeed(): void;
+    function setI2CSpeed(speed): void;
 
 }
