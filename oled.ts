ec5c05f768a99c84453440c8d23cd80db57cddd5
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-29 18:50:34 +1000

    testing rotations

--- oled.ts
+++ oled.ts
@@ -349,6 +349,7 @@ namespace OLED {
     export function orientWest() {
         orientation = WEST
         cmd1(0xc0)       // SSD1306_COMSCANDEC
+        cmd1(0xA1)
         cmd2(0x20, 0x01) // SSD1306_MEMORYMODE
         MAX_Y = 63
         MAX_X = 127
