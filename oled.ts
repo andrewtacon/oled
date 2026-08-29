dcad2d27412121abef9c64a336e73b9abb2b0b2c
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-29 18:07:08 +1000

    again

--- oled.ts
+++ oled.ts
@@ -395,8 +395,9 @@ namespace OLED {
     //% weight=10 blockGap=8
     export function orientEast() {
         //fails  c8 a1
+        //fails  c8 a0
         orientation = EAST
-        cmd1(0xc8)       // SSD1306_COMSCANDEC
+        cmd1(0xc0)       // SSD1306_COMSCANDEC
         cmd1(0xA0)
         cmd2(0x20, 0x01) // SSD1306_MEMORYMODE
         MAX_X = 127
