67602a0ddfec3a967c1d57fa198727cec5c544e7
Andrew Tacon <{ID}+{username}@users.noreply.github.com>
2026-08-29 16:29:15 +1000

    guessing that orientation is causing problem from char

--- oled.ts
+++ oled.ts
@@ -158,7 +158,9 @@ namespace OLED {
                 // Draw the pixel using the adaptive pixel helper
                 // pixel(col + i, (row * 8) + bit, drawColor)
                 if (orientation === VERTICAL) {
+                    orientation = HORIZONTAL
                     pixel((col) + bit, (row + i), drawColor)
+                    orientation = VERTICAL
                 } else {
                     pixel((col) + i, (row + bit), drawColor)         
                 }
